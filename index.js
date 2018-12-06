6bf8e6f30855259e425a2d692ac6436873e9b353
eyu <eyu@cse21712.cs.local>
2018-12-06 12:26:04 -0500

    added classy things. Varying vel/pos not working

--- index.js
+++ index.js
@@ -14,14 +14,21 @@ var done = false;
 
 
 twgl.resizeCanvasToDisplaySize(gl.canvas);
-var cube_info = {
-  pos: [0,0,0],
-  rot_x: 0,
-  rot_y: 0,
-  rot_z: 0,
-  scale: 0.5,
-  color: [1, 1, 0, 1],
-}
+
+var scene_objs = [];
+
+// var cube_info = {
+//   pos: [0,0,0],
+//   rot_x: 0,
+//   rot_y: 0,
+//   rot_z: 0,
+//   scale: 0.5,
+//   color: [1, 1, 0, 1],
+//   buffer: cubeBuffer
+// }
+let a = new Cube();
+a.pos = [0,1,0]
+scene_objs.push(a);
 
 // cube_info = {
 //   pos: [0,0,0],
@@ -30,24 +37,31 @@ var cube_info = {
 //   color: [1, 1, 0, 1],
 // }
 
-var pyr_info = {
-  pos: [0,0,-.5],
-  // dir: [1,1,1],
-  rot_x: 0,
-  rot_y: 0,
-  rot_z: 0,
-  scale: 0.5,
-  color: [1, 0, 0, 1],
-}
+// let p = {
+//   pos: [2,0,-10],
+//   // dir: [1,1,1],
+//   rot_x: 0,
+//   rot_y: 0,
+//   rot_z: 0,
+//   scale: 0.5,
+//   color: [1, 0, 0, 1],
+//   buffer: pyrBuffer
+// }
+
+// let p = new Pyramid();
+// scene_objs.push(p);
 
 var camera_info = {
-  pos: [0,0,5],
+  pos: [0,0,100],
   tar: [0,0,0],
   up: [0,1,0],
   fov: 60 * 3.14 / 180,
-  zNear: 0.1,
-  zFar: 100,
+  zNear: 1,
+  zFar: 2000,
 };
+
+let flock = new Flock();
+flock.addBirds();
 
 function tick(time) {
   render(time);
@@ -57,10 +71,8 @@ function tick(time) {
 
 function update(time){
   time *= 0.0005;
-  pyr_info.rot_x = time * 1.2;
-  pyr_info.rot_y = -time;
-  // cube_info.pos[2] -= .001
-  // cube_info.pos[1] -= .001
+  flock.birds.forEach(b => b.update());
+  flock.update();
 
   moveCamera();
 }
@@ -87,8 +99,7 @@ function moveCamera() {
 
 }
 
-function render(time) {
-
+function render() {
   twgl.resizeCanvasToDisplaySize(canvas);
   gl.viewport(0, 0, canvas.width, canvas.height);
   const aspect = canvas.clientWidth / canvas.clientHeight;
@@ -98,39 +109,29 @@ function render(time) {
   gl.useProgram(programInfo.program);
 
   gl.enable(gl.CULL_FACE);
-  // gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.DEPTH_TEST);
 
-  let uniforms = {
-    view_matrix: view_matrix,
-    proj_matrix: proj_matrix,
-    model_matrix: model_matrix(cube_info),
-    color: cube_info.color,
-  };
-  twgl.setBuffersAndAttributes(gl, programInfo, cubeBuffer);
-  twgl.setUniforms(programInfo, uniforms);
-  twgl.drawBufferInfo(gl, cubeBuffer);
-
-  gl.useProgram(programInfo.program);
-
-  uniforms = {
-    view_matrix: view_matrix,
-    proj_matrix: proj_matrix,
-    model_matrix: model_matrix(pyr_info),
-    color: pyr_info.color,
-  };
-  twgl.setBuffersAndAttributes(gl, programInfo, pyrBuffer);
-  twgl.setUniforms(programInfo, uniforms);
-  twgl.drawBufferInfo(gl, pyrBuffer);
+  for (let obj_info of scene_objs) {
+    let uniforms = {
+      view_matrix: view_matrix,
+      proj_matrix: proj_matrix,
+      model_matrix: model_matrix(obj_info),
+      color: obj_info.color,
+    };
+    twgl.setBuffersAndAttributes(gl, programInfo, obj_info.buffer);
+    twgl.setUniforms(programInfo, uniforms);
+    twgl.drawBufferInfo(gl, obj_info.buffer);
+  }
 }
 
 function model_matrix(info) {
   var T = m4.translation(info.pos);
   var S = m4.scaling([info.scale, info.scale, info.scale]);
   var R;
-  if (info.rot_x != null) {
-    var Rx = m4.rotationX(info.rot_x);
-    var Ry = m4.rotationY(info.rot_y);
-    var Rz = m4.rotationZ(info.rot_z);
+  if (info.drawRotation) {
+    var Rx = m4.rotationX(info.rot[0]);
+    var Ry = m4.rotationY(info.rot[1]);
+    var Rz = m4.rotationZ(info.rot[2]);
     R = m4.multiply(Rx, m4.multiply(Ry, Rz));
   } else {
     R = m4.identity()
@@ -156,3 +157,80 @@ onkeyup = e => {
   keys[k] = false;
 }
 requestAnimationFrame(tick);
+
+function addVecRandomUniform(vec, amt=1) {
+  vec[0] += (Math.random() - Math.random()) * amt;
+  vec[1] += (Math.random() - Math.random()) * amt;
+  vec[2] += (Math.random() - Math.random()) * amt;
+  return vec;
+}
+
+function Flock() {
+  this.birds = [];
+  this.pos = [0,0,0];
+  this.vel = [1,0,0];
+  this.addBirds = function(n=10) {
+    for (let i = 0; i < n; i++){
+      let b = new Bird();
+      b.vel = flock.vel;
+      b.solid.pos = flock.pos;
+      b.vel = addVecRandomUniform(b.vel, .01);
+      b.solid.pos = addVecRandomUniform(b.solid.pos, 10);
+
+      console.log(b.vel, b.solid.pos)
+
+      b.solid.color = [Math.random(), Math.random(), Math.random(), 1]
+
+      this.birds.push(b);
+    }
+  }
+  this.update = function() {
+    let n = this.birds.length;
+    if (n <= 0) return 
+
+    //calculate flock center & velection
+    let pos = [0,0,0];
+    let vel = [0,0,0];
+    for (let b of this.birds) {
+      pos = v3.add(b.solid.pos, pos);
+      vel = v3.add(b.vel, vel);
+    }
+    this.pos = v3.divScalar(pos, n);
+    this.vel = v3.divScalar(vel, n);
+    
+  }
+}
+
+function Bird() {
+  this.solid = new Pyramid();
+  // each bird is responsible for making sure it gets drawn
+  scene_objs.push(this.solid);
+  this.vel = [0,0,1];
+  this.update = function() {
+    this.solid.move(this.vel)
+    console.log(this.vel)
+  }
+}
+
+function Solid(buffer) {
+  this.pos = [0,0,0];
+  this.dir = [0,0,1];
+  this.scale = 1;
+  this.color = [1, 1, 0, 1];
+  this.buffer = buffer;
+  
+  this.drawRotation = false;
+  this.rot = [0,0,0];
+  
+  this.move = function(v) {
+    v3.add(this.pos, v, this.pos);
+  }
+}
+
+function Cube() {
+  Solid.call(this, cubeBuffer)
+}
+
+function Pyramid() {
+  Solid.call(this, pyrBuffer)
+}
